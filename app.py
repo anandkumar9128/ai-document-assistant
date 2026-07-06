550e5b4ff7e0e0148aeb04c4a1aa7ba563bd1d67
anandkumar9128 <anandkumargrd11@gmail.com>
2026-07-07 02:31:10 +0530

    Fix LangChain text splitter import path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
